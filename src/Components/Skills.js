916e6daf52bc9f2f667fe7507215895db71d79e1
sekul <sekulhassan3031@gmail.com>
2024-07-08 15:05:34 +0600

    skill config

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import {Container} from "react-bootstrap";
+import skillConfig from "../Config/SkillConfig";
 
 function Skills(props) {
     return (
         <Container className="mt-3">
-            <p className="description"> <span className="globalTitle">Problem Solving : </span>About 1000+ problem are solved. Including Codeforces,AtCoder,LetCode,CSES.</p>
-            <p className="description"> <span className="globalTitle">Algorithm : </span>Basic to intermediate knowledge of DSA.</p>
-            <p className="description"> <span className="globalTitle">Git & Github : </span> I can use git and github, i know how to mange git branches,git actions including CI/CD.</p>
-            <p className="description"> <span className="globalTitle">Docker : </span> I know basic of docker.</p>
-            <p className="description"> <span className="globalTitle">Managing Cloud Server : </span>I have experience to manage digital ocean cloud server with live project which is ncpc 2023 registration website.</p>
-            <p className="description"> <span className="globalTitle">Team Work :</span>I completed one full project with my team. From this project i learnt how can possible to work in huge pressure.</p>
-            <p className="description"> <span className="globalTitle">Languages : </span>C, C++, Java, Javascript, Python.</p>
-            <p className="description"> <span className="globalTitle">Frameworks  : </span>React Js, Spring Boot, Bootstrap and React Bootstrap as a CSS framework</p>
-            <p className="description"> <span className="globalTitle">API Integration : </span>How to integrate bKash, Nagad apis in projects, APIs creating, integration, and manage. </p>
-            <p className="description"> <span className="globalTitle">Managing Project :</span>I have a startup (Recursive Education) and it's managed by me.</p>
+            {
+                skillConfig && skillConfig.map((skill,idx)=> (
+                    <p className="description"><span className="globalTitle">{skill.name}</span>{skill.description}</p>
+                ))
+            }
         </Container>
     );
 }
---
+++ src/Config/SkillConfig.js
@@ -0,0 +1,55 @@
+
+
+const skillConfig = [
+    {
+        id: 1,
+        name: "Problem Solving : ",
+        description: "About 1000+ problem are solved. Including Codeforces,AtCoder,LetCode,CSES.",
+    },
+    {
+        id: 2,
+        name: "Algorithm : ",
+        description: "Basic to intermediate knowledge of DSA.",
+    },
+    {
+        id: 3,
+        name: "Git & Github : ",
+        description: "I can use git and github, i know how to mange git branches,git actions including CI/CD."
+    },
+    {
+        id: 4,
+        name: "Docker : ",
+        description: "I know basic of docker."
+    },
+    {
+        id: 5,
+        name: "Managing Cloud Server : ",
+        description: "I have experience to manage digital ocean cloud server with live project which is ncpc 2023 registration website."
+    },
+    {
+        id: 6,
+        name: "Team Work : ",
+        description: "I completed one full project with my team. From this project i learnt how can possible to work in huge pressure."
+    }, {
+        id: 7,
+        name: "Languages : ",
+        description: "C, C++, Java, Javascript, Python."
+    },
+    {
+        id: 8,
+        name: "Frameworks : ",
+        description: "React Js, Spring Boot, Bootstrap and React Bootstrap as a CSS framework."
+    },
+    {
+        id: 9,
+        name: "API Integration : ",
+        description: "How to integrate bKash, Nagad apis in projects, APIs creating, integration, and manage."
+    },
+    {
+        id: 10,
+        name: "Managing Project : ",
+        description: "I have a startup (Recursive Education) and it's managed by me."
+    },
+]
+
+export default skillConfig;
