3cb95747621a2bd00144d64bb9ae7cc9e819ff26
sekul <sekulhassan3031@gmail.com>
2024-07-08 20:31:09 +0600

    project config done

--- src/Components/TopMost.js
+++ src/Components/TopMost.js
@@ -3,23 +3,26 @@ import {Card, CardBody, CardFooter, Col, Container, Row} from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
 import react from "../Assets/Images/react.jpg";
 import java from "../Assets/Images/java.jpg";
+import projectConfig from "../Config/ProjectConfig";
 
 function TopMost(props) {
     const text1 = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. At explicabo laboriosam nam, officiis reprehenderit sapiente."
     const text2 = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. At explicabo laboriosam nam, officiis reprehenderit sapiente."
+
+    const threeProjects = projectConfig.slice(0,2);
+
     return (
        <Container fluid="true" className="mt-4 mx-3">
            <Row>
                <h4 className="title">Top most three projects</h4>
-               <Col lg={4} md={4} sm={6}>
-                  <ProjectCard image={react} text={text1}/>
-               </Col>
-               <Col lg={4} md={4} sm={6}>
-                   <ProjectCard image={react} text={text2}/>
-               </Col>
-               <Col lg={4} md={4} sm={6}>
-                   <ProjectCard image={java} text={text1}/>
-               </Col>
+               {
+                   threeProjects && threeProjects.map((project,idx)=>(
+                       <Col lg={4} md={4} sm={6}>
+                           <ProjectCard image={project.demoImage} text={project.description} projectId={project.projectId}/>
+                       </Col>
+                   ))
+               }
+
            </Row>
        </Container>
     );
